7d5f2a5fb8bf9a99abd2f9d9a860d50d803e442a
Daniel McNally <mcnallydp@gmail.com>
2018-02-13 11:52:41 -0500

    1.0.0-alpha.1
    Basic functionality, tests, linting, and documentation.

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# node-jsonrpc-server
-A simple and lightweight library for creating a JSON-RPC 2.0 compliant HTTP server
+# jsonrpc-server
+
+A simple and lightweight library for creating a JSON-RPC 2.0 compliant HTTP server.
+
+Complies with the [JSON-RPC 2.0](http://www.jsonrpc.org/specification) and the [JSON-RPC 2.0 Transport: HTTP](https://www.simple-is-better.org/json-rpc/transport_http.html) specifications.
+
+## Install
+
+To install jsonrpc-server in the current directory, run:
+
+```bash
+npm install jsonrpc-server --save
+```
+
+## Usage
+
+```javascript
+const jsonrpc = require('jsonrpc-server');
+
+function sum(arr) {
+  let total = 0;
+  for (let n = 0; n < arr.length; n += 1) {
+    total += arr[n];
+  }
+  return total;
+}
+
+async function wait(params) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, params.ms);
+  });
+}
+
+jsonrpc.setMethod('sum', sum);
+jsonrpc.setMethod('wait', wait);
+jsonrpc.listen(9090); // listening on http://127.0.0.1:9090/
+```
+
+You can also specify a path to listen on:
+
+```javascript
+jsonrpc.listen(9090, '/api') // listening on http://127.0.0.1:9090/api
+```
+
+## Example Requests
+
+Here are some example requests made against the server created above:
+
+### Sum
+
+```
+POST / HTTP/1.1
+Host: 127.0.0.1:9090
+Content-Type: application/json
+Accept: application/json
+Content-Length: 56
+
+{"jsonrpc":"2.0","id":1,"method":"sum","params":[1,2,3]}
+```
+
+```
+connection: close
+content-type: application/json
+content-length: 35
+
+ {"jsonrpc":"2.0","id":1,"result":6}
+```
+
+### Sum (Batched)
+
+```
+POST / HTTP/1.1
+Host: 127.0.0.1:9090
+Content-Type: application/json
+Accept: application/json
+Content-Length: 115
+
+[{"jsonrpc":"2.0","id":1,"method":"sum","params":[1,2,3]},{"jsonrpc":"2.0","id":2,"method":"sum","params":[4,5,6]}]
+```
+
+```
+connection: close
+content-type: application/json
+content-length: 74
+
+[{"jsonrpc":"2.0","id":1,"result":6},{"jsonrpc":"2.0","id":2,"result":15}]
+```
+
+### Wait
+
+```
+POST / HTTP/1.1
+Host: 127.0.0.1:9090
+Content-Type: application/json
+Accept: application/json
+Content-Length: 59
+
+{"jsonrpc":"2.0","id":1,"method":"wait","params":{"ms":50}}
+```
+
+```
+connection: close
+content-type: application/json
+content-length: 38
+
+{"jsonrpc":"2.0","id":1,"result":true}
+```
---
+++ test/test.js
@@ -0,0 +1,170 @@
+/* eslint-env mocha */
+
+const assert = require('assert');
+const request = require('supertest');
+const jsonrpc = require('../lib/jsonrpc-server');
+
+const { server } = jsonrpc;
+
+function sum(arr) {
+  let total = 0;
+  for (let n = 0; n < arr.length; n += 1) {
+    total += arr[n];
+  }
+  return total;
+}
+
+async function wait(params) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, params.ms);
+  });
+}
+
+describe('jsonrpc-server', () => {
+  before(async () => {
+    jsonrpc.setMethod('sum', sum);
+    jsonrpc.setMethod('wait', wait);
+  });
+
+  it('should 404 an unknown path', () => request(server)
+    .post('/invalidpath')
+    .expect(404));
+
+  it('should 405 GET requests', () => request(server)
+    .get('/')
+    .expect(405));
+
+  it('should 415 non application/json requests', () => request(server)
+    .post('/')
+    .expect(415));
+
+  it('should error invalid json', () => request(server)
+    .post('/')
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .send('asdlkfjasld')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .then((response) => {
+      assert.strictEqual(response.body.jsonrpc, '2.0');
+      assert.strictEqual(response.body.result, undefined);
+      assert.strictEqual(response.body.error.code, jsonrpc.PARSE_ERROR);
+    }));
+
+  it('should error invalid jsonrpc', () => request(server)
+    .post('/')
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .send('{"jsonrpc":"1.0","id":1,"method":"sum"}')
+    .expect(200)
+    .then((response) => {
+      assert.strictEqual(response.body.jsonrpc, '2.0');
+      assert.strictEqual(response.body.result, undefined);
+      assert.strictEqual(response.body.id, 1);
+      assert.strictEqual(response.body.error.code, jsonrpc.INVALID_REQUEST);
+    }));
+
+  it('should error invalid id', () => request(server)
+    .post('/')
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .send('{"jsonrpc":"2.0","id":["ids should not be arrays"],"method":"sum"}')
+    .expect(200)
+    .then((response) => {
+      assert.strictEqual(response.body.jsonrpc, '2.0');
+      assert.strictEqual(response.body.result, undefined);
+      assert.strictEqual(response.body.error.code, jsonrpc.INVALID_REQUEST);
+    }));
+
+  it('should error invalid method', () => request(server)
+    .post('/')
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .send('{"jsonrpc":"2.0","id":1,"method":123}')
+    .expect(200)
+    .then((response) => {
+      assert.strictEqual(response.body.jsonrpc, '2.0');
+      assert.strictEqual(response.body.result, undefined);
+      assert.strictEqual(response.body.id, 1);
+      assert.strictEqual(response.body.error.code, jsonrpc.METHOD_NOT_FOUND);
+    }));
+
+  it('should error invalid params', () => request(server)
+    .post('/')
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .send('{"jsonrpc":"2.0","id":1,"method":"sum","params":"params should not be a string"}')
+    .expect(200)
+    .then((response) => {
+      assert.strictEqual(response.body.jsonrpc, '2.0');
+      assert.strictEqual(response.body.result, undefined);
+      assert.strictEqual(response.body.id, 1);
+      assert.strictEqual(response.body.error.code, jsonrpc.INVALID_PARAMS);
+    }));
+
+  it('should return expected result for valid request', () => request(server)
+    .post('/')
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .send('{"jsonrpc":"2.0","id":1,"method":"sum","params":[1,2,3]}')
+    .expect(200)
+    .then((response) => {
+      assert.strictEqual(response.body.jsonrpc, '2.0');
+      assert.strictEqual(response.body.error, undefined);
+      assert.strictEqual(response.body.id, 1);
+      assert.strictEqual(response.body.result, 6);
+    }));
+
+  it('should return nothing for notifications', () => request(server)
+    .post('/')
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .send('{"jsonrpc":"2.0","method":"sum","params":[1,2,3]}')
+    .expect(204)
+    .then((response) => {
+      assert.strictEqual(response.body, '');
+    }));
+
+  it('should return batched results for valid requests', () => request(server)
+    .post('/')
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .send('[{"jsonrpc":"2.0","id":1,"method":"sum","params":[1,2,3]},{"jsonrpc":"2.0","id":2,"method":"sum","params":[4,5,6]}]')
+    .expect(200)
+    .then((response) => {
+      assert.ok(Array.isArray(response.body));
+      assert.strictEqual(response.body.length, 2);
+      for (let n = 0; n < response.body.length; n += 1) {
+        assert.strictEqual(response.body[n].jsonrpc, '2.0');
+        assert.strictEqual(response.body[n].error, undefined);
+        if (response.body[n].id === 1) {
+          assert.strictEqual(response.body[n].result, 6);
+        } else if (response.body[n].id === 2) {
+          assert.strictEqual(response.body[n].result, 15);
+        } else {
+          assert.fail('unexpected id value');
+        }
+      }
+      assert.strictEqual(response.body[0].jsonrpc, '2.0');
+      assert.strictEqual(response.body[0].error, undefined);
+      assert.strictEqual(response.body[0].result, 6);
+      assert.strictEqual(response.body[1].jsonrpc, '2.0');
+      assert.strictEqual(response.body[1].error, undefined);
+      assert.strictEqual(response.body[1].result, 15);
+    }));
+
+  it('should call an async method', () => request(server)
+    .post('/')
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .send('{"jsonrpc":"2.0","id":1,"method":"wait","params":{"ms":50}}')
+    .expect(200)
+    .then((response) => {
+      assert.strictEqual(response.body.jsonrpc, '2.0');
+      assert.strictEqual(response.body.error, undefined);
+      assert.strictEqual(response.body.id, 1);
+      assert.ok(response.body.result);
+    }));
+});
